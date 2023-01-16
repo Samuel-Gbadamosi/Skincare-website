6f154525e7e03ee690f0b254e4f46948a80eda64
samuel <damosisamuel@gmail.com>
2023-01-16 15:59:51 +0100

    g

--- sitonuovo/blog.php
+++ sitonuovo/blog.php
@@ -95,7 +95,6 @@
             </div>
 
           </section>
-
           <section class="sec9">
             <div class="row">
               <div class="col-3 pt-4">
@@ -175,13 +174,11 @@
    
 
 </main>
-    <!-- main -->
-<!-- footer -->
+    <!-- main ends -->
+<!-- footer starts -->
   <?php include('footer.php'); ?>
-    <!-- footer -->
+    <!-- footer ends -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
-    
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
