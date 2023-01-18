d5b2734d10dc3aa94584ebc13fcd38e1bc155796
samuel <damosisamuel@gmail.com>
2023-01-18 14:52:14 +0100

    c

--- sitonuovo/index.php
+++ sitonuovo/index.php
@@ -18,9 +18,9 @@
         <!-- css link -->
     <!-- cdn -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- cdn -->
+    <!-- cdn link -->
   </head>
-  <!-- body starts -->
+  <!-- body starts ends -->
 <body class="container-fluid">
     <!-- header -->
     <!-- <header class="container-fluid ">
