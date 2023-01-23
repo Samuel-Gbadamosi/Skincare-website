16e73a9ca69fafbb556a4e3eaf6e16e37f3e8733
samuel <damosisamuel@gmail.com>
2023-01-23 16:22:31 +0100

    JS

--- sitonuovo/script.js
+++ sitonuovo/script.js
@@ -2,7 +2,10 @@ function choose(){
 var x = Math.random();
     var result = (Math.floor(x * 25));
     var message = 'coming soon in';
-    var name = document.getElementById('chy').innerTEXT = alert( message + ' ' +  result + 'HRS');
+    var d = new Date();
+    var date =  d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+
+    var name = document.getElementById('chy').innerTEXT = alert( message + ' ' +  result + ' DAYS' + ' ' + date);
 
     return name;
 }
