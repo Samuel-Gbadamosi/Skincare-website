cb59b71fb3c053663b311f256eb50679e773d2d1
samuel <damosisamuel@gmail.com>
2023-01-23 16:08:57 +0100

    JS ADDED

---
+++ sitonuovo/script.js
@@ -0,0 +1,8 @@
+function choose(){
+var x = Math.random();
+    var result = (Math.floor(x * 25));
+    var message = 'coming soon in';
+    var name = document.getElementById('chy').innerTEXT = alert( message + ' ' +  result + 'HRS');
+
+    return name;
+}
